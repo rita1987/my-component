0e2fd694c476cee5f22fe11c4c823ae9c83d06f6
zp <1104891207@qq.com>
2018-01-22 17:27:06 +0800

    update

--- build/build.js
+++ build/build.js
@@ -1,11 +1,14 @@
 require('./check-versions')()
 
+//process：代表当前Node.js进程。通过process对象可以拿到许多有用信息，作为一个对象，它对于 Node.js 应用程序始终是可用的，故无需使用 require()。
+//process.env属性返回一个包含用户环境信息的对象
+//在Windows系统下，环境变量是不区分大小写的
 process.env.NODE_ENV = 'production'
 
-var ora = require('ora')
-var rm = require('rimraf')
-var path = require('path')
-var chalk = require('chalk')
+var ora = require('ora') //实现node.js 命令行环境的 loading效果， 和显示各种状态的图标等
+var rm = require('rimraf')  //删除已发布文件  rimraf(f, [opts], callback)
+var path = require('path') //path 模块提供了一些工具函数，用于处理文件与目录的路径
+var chalk = require('chalk') //颜色插件
 var webpack = require('webpack')
 var config = require('../config')
 var webpackConfig = require('./webpack.prod.conf')
@@ -18,6 +21,7 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     webpack(webpackConfig, function (err, stats) {
         spinner.stop()
         if (err) throw err
+        //process.stdout 属性返回连接到 stdout (fd 1)的流
         process.stdout.write(stats.toString({
                 colors: true,
                 modules: false,
--- src/page/manage.vue
+++ src/page/manage.vue
@@ -1,21 +1,6 @@
 <template>
     <div class="manage_page fillcontain">
-        <el-container>
-            <el-header>
-                <el-menu mode="horizontal" @select="handleSelect">
-                    <el-menu-item index="1">首页</el-menu-item>
-                    <el-submenu>
-                        <template slot="title">静态资源</template>
-                        <el-menu-item index="2-1">local JS</el-menu-item>
-                        <el-menu-item index="2-2">第三方插件</el-menu-item>
-                    </el-submenu>
-                </el-menu>
 
-            </el-header>
-            <el-aside>
-
-            </el-aside>
-        </el-container>
     </div>
 </template>
 
@@ -26,11 +11,6 @@
                 return this.$route.path.replace('/', '');
             }
         },
-        methods: {
-            handleSelect(key, keyPath) {
-                console.log(key, keyPath);
-            }
-        }
     }
 </script>
 
